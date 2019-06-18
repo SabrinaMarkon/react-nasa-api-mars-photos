47f703adddc802981f9c5cd5f5e9d2defbaf7468
Sabrina Markon <phpsitescripts@outlook.com>
2019-06-17 21:59:42 -0600

    Folder structure.

--- README.old.md
+++
@@ -1,2 +0,0 @@
-# react-nasa-api-mars-photos
-Use the NASA Mars photo API to provide users with the ability to search for photos taken by NASA's Curiosity Mars rover.
---
+++ src/containers/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hurr-dee-durr
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
