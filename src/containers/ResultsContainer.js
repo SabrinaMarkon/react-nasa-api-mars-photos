ac8ff861831d5794482af5d098a36d198a80d13e
Sabrina Markon <phpsitescripts@outlook.com>
2019-06-24 01:47:40 -0600

    Add animated shine across grid-items onMouseOver.

--- src/containers/ResultsContainer.js
+++ src/containers/ResultsContainer.js
@@ -2,18 +2,47 @@ import React, { Component } from 'react';
 
 export default class ResultsContainer extends Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      shineclass: ''
+    }
+    this.handleHover = this.handleHover.bind(this);
+    this.handleLeave = this.handleLeave.bind(this);
+  }
+
+  handleHover(event) {
+    this.setState({
+      shineclass: 'shine'
+    });
+    // console.log(event.target.id + ' - ' + this.state.shineclass);
+  }
+
+  handleLeave() {
+    setTimeout(() => {
+      this.setState({
+        shineclass: ''
+      });
+    }, 2000);
+  }
+
   render() {
     let searchResults =this.props.searchResults;
     let results = searchResults.map((row,i) => {
       return (
-          <figure className="grid-item" key={i}>
+        <div className="grid-item-wrapper" id={i} key={i} onMouseOver={this.handleHover} onMouseLeave={this.handleLeave}>
+          <figure className="grid-item">
             <img src={row['img_src']} className="photo" alt={row['img_src']} />
+            {this.state.shineclass === 'shine'
+              && <div className='shine'></div>
+            }
             <figcaption className="description">
               <div>Martian Sol: <span className="fieldvalue">{row['sol']}</span></div>
               <div>Earth Date: <span className="fieldvalue">{row['earth_date']}</span></div>
               <div>Camera: <span className="fieldvalue">{row['camera'].name}</span></div>
             </figcaption>
           </figure>
+        </div>
       );
     }
     );
