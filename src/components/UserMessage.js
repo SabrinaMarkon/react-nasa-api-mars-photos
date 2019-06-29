1de609ce55f0fa69c283f0f1d4ba3f9964d9675f
Sabrina Markon <phpsitescripts@outlook.com>
2019-06-29 00:07:36 -0600

    userMessage for errors should be stringified so we don't get the JSX child cannot be an object error.

--- src/components/UserMessage.js
+++ src/components/UserMessage.js
@@ -2,9 +2,6 @@ import React from 'react';
 
 export default function UserMessage(props) {
   let userMessage = JSON.stringify(props.userMessage);
-  // if (props.userMessage.length > 1) {
-  //   userMessage = 'No Results Found';
-  // }
   return (
     <div className="user-message">{userMessage}</div>
   );
