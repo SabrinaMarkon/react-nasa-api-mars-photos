4b0fcd5d9c137f3dafc054a501e10f07b9b948a6
Sabrina Markon <phpsitescripts@outlook.com>
2019-08-25 04:54:36 -0600

    Check userMessage length to avoid error.

--- src/components/UserMessage.js
+++ src/components/UserMessage.js
@@ -2,8 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default function UserMessage (props) {
+    let userMessage = props.userMessage;
+    if (props.userMessage.length > 1) {
+        userMessage = 'No Results Found';
+    }
     return (
-        <div className="user-message">{props.userMessage}</div>
+        <div className="user-message">{userMessage}</div>
     );
 }
 
